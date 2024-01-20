cd158cbf2011a9b0ca5f5f3d861ed5ebc6427a69
andrea <andrea.thouvenot@le-campus-numerique.fr>
2024-01-20 15:00:26 +0100

    header avec head

---
+++ header.php
@@ -0,0 +1,20 @@
+// HEAD pour toutes les page
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <description> <?php echo $MetaDescription ?> </description>
+    <link rel="stylesheet" href="style.php" />
+    <title> <?php  echo $MetaTitle ?> </title>
+</head>
+// HEADER pour toutes les pages
+<header>
+    <div>
+        <a href="cv.php">Mon CV</a>
+    </div>
+    <div>
+        <a href="hobby.php">Mon Hobby</a>
+    </div>
+    <div>
+        <a href="contact.php">Contact</a>
+    </div>
+</header>
